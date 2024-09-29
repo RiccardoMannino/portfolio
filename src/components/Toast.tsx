6a02d96c2ff9b3419a594ad1a2a9f27e5a5abbf7
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2024-09-29 02:27:06 +0200

    git commit -m "Stopped tracking .env File"

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Toast() {
   return (
     <div className="absolute bottom-3 left-[75%] flex h-14 w-52 items-center rounded-lg border bg-neutral-200 pl-3">
-      Messaggio...
+      Messaggio....
     </div>
   )
 }
