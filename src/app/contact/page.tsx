1957fb03bf083d2e1d8d1a7062d54bc929c198c5
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2024-10-19 03:13:56 +0200

    validazione e errori

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,27 +3,21 @@
 import { useEffect, useState } from 'react'
 import { getCalApi } from '@calcom/embed-react'
 import emailjs from '@emailjs/browser'
-
+import { useForm } from 'react-hook-form'
 import toast from 'react-hot-toast'
 
 export default function Contact() {
   const [IsSending, setIsSending] = useState(false)
 
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  })
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm()
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    const { name, value } = e.target
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }))
-  }
+  const formValue = watch()
 
   useEffect(() => {
     async function prenotation() {
@@ -37,16 +31,14 @@ export default function Contact() {
     prenotation()
   }, [])
 
-  const sendEmail = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-
+  const sendEmail = async (data) => {
     try {
       setIsSending(true)
-      const result = await emailjs.sendForm(
-        process.env.EMAILJS_SERVICE_ID as string,
-        process.env.EMAILJS_TEMPLATE_ID as string,
-        e.target as HTMLFormElement,
-        process.env.EMAILJS_PUBLIC_KEY as string,
+      const result = await emailjs.send(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID as string,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID as string,
+        data,
+        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY as string,
       )
       console.log(result)
 
@@ -70,14 +62,9 @@ export default function Contact() {
           Errore nell&apos; invio del messaggio ⛔
         </div>
       ))
-      // toast.error("Errore nell'invio del messaggio.")
     } finally {
-      setIsSending(false),
-        setFormData({
-          name: '',
-          email: '',
-          message: '',
-        })
+      setIsSending(false)
+      reset()
     }
   }
 
@@ -91,40 +78,68 @@ export default function Contact() {
         prenota una chiamata.
       </p>
 
-      <form onSubmit={sendEmail}>
-        <div className="mt-7 flex flex-col gap-5 sm:grid sm:grid-cols-2 sm:grid-rows-[40px]">
-          <input
-            disabled={IsSending}
-            type="text"
-            placeholder="Nome"
-            name="name"
-            value={formData.name}
-            required
-            onChange={handleChange}
-            className="rounded-xl border bg-neutral-100 px-2 py-2 indent-3 text-sm text-neutral-500 focus:border-gray-400 focus:outline-none focus:ring focus:ring-gray-300 md:text-base lg:text-lg"
-          />
-          <input
-            disabled={IsSending}
-            type="email"
-            placeholder="Email"
-            name="email"
-            value={formData.email}
-            required
-            onChange={handleChange}
-            className="rounded-xl border bg-neutral-100 px-2 py-2 indent-3 text-sm text-neutral-500 focus:border-gray-400 focus:outline-none focus:ring focus:ring-gray-300 md:text-base lg:text-lg"
-          />
+      <form onSubmit={handleSubmit(sendEmail)}>
+        <div
+          className={`sm:grid-auto-rows-min mt-7 flex flex-col gap-2 sm:grid sm:grid-cols-2`}
+        >
+          <div className="w-full">
+            <input
+              disabled={IsSending}
+              type="text"
+              placeholder="Nome e Cognome"
+              {...register('name', {
+                required: true,
+                maxLength: 50,
+                pattern: /^[A-ZÀ-ÖØ-Ý][a-zà-öø-ÿ']+\s[A-ZÀ-ÖØ-Ý][a-zà-öø-ÿ']+$/,
+              })}
+              className={` ${(errors.name && 'border-red-500 bg-red-100 focus:outline-none focus:ring focus:ring-red-300') || 'bg-neutral-100'} w-full rounded-xl border bg-neutral-100 px-2 py-2 indent-3 text-sm text-neutral-500 focus:border-gray-400 focus:outline-none focus:ring focus:ring-gray-300 md:text-base lg:text-lg`}
+            />
+            {errors.name && (
+              <p className="mb-2 grid pt-1 text-sm text-red-500 md:text-base lg:text-lg">
+                Nome o cognome non validi
+              </p>
+            )}
+          </div>
 
-          <textarea
-            disabled={IsSending}
-            className="col-span-2 rounded-xl border bg-neutral-100 pt-3 indent-3 text-sm text-neutral-500 focus:border-gray-400 focus:outline-none focus:ring focus:ring-gray-300 md:text-base lg:text-lg"
-            rows={10}
-            placeholder="Inserisci il tuo messaggio"
-            required
-            onChange={handleChange}
-            name="message"
-            value={formData.message}
-            maxLength={250}
-          />
+          <div className="w-full">
+            <input
+              disabled={IsSending}
+              type="email"
+              placeholder="Email"
+              {...register('email', {
+                required: true,
+                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              })}
+              className={`w-full rounded-xl border bg-neutral-100 px-2 py-2 indent-3 text-sm text-neutral-500 focus:border-gray-400 focus:outline-none focus:ring focus:ring-gray-300 valid:focus:ring md:text-base lg:text-lg ${(errors.email && 'border-red-500 bg-red-100') || 'bg-neutral-100'}`}
+            />
+            {errors.email && (
+              <p className="mb-5 pt-1 text-sm text-red-500 md:text-base lg:text-lg">
+                Email non valida
+              </p>
+            )}
+          </div>
+
+          <div className="col-span-2 w-full">
+            <textarea
+              disabled={IsSending}
+              className={`w-full rounded-xl border bg-neutral-100 pt-3 indent-3 text-sm text-neutral-500 focus:border-gray-400 focus:outline-none focus:ring focus:ring-gray-300 md:text-base lg:text-lg ${(errors.message && 'border-red-500 bg-red-100') || 'bg-neutral-100'}`}
+              rows={10}
+              placeholder="Inserisci il tuo messaggio"
+              {...register('message', {
+                required: true,
+                minLength: 50,
+                maxLength: 250,
+              })}
+            />
+            <p className="mt-2">
+              Caratteri inviati: {formValue.message?.length || 0}
+            </p>
+            {errors.message && (
+              <p className="mb-2 text-sm text-red-500 md:text-base lg:text-lg">
+                Inserisci un messaggio di almeno 50 caratteri
+              </p>
+            )}
+          </div>
         </div>
         <div className="mt-10 flex w-full justify-end">
           <button
