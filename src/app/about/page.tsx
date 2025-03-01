79af5191a297b76acbcc877bc9a55cd2a182e91a
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2025-03-01 02:31:15 +0100

    modifica testo pagina about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -26,37 +26,37 @@ export default function About() {
           className="rounded-lg phone:hidden"
         />
       </div>
-      <p className="mt-5 text-lg text-neutral-500 md:text-xl">
+      <p className="mt-5 text-lg text-neutral-500">
         Mi chiamo Riccardo Mannino e sono un appassionato frontend developer con
         un forte interesse per la creazione di interfacce web eleganti e
         funzionali.
       </p>
-      <p className="mt-5 text-lg text-neutral-500 md:text-xl">
+      <p className="mt-5 text-lg text-neutral-500">
         Durante la mia carriera ho sviluppato competenze in HTML, CSS,
         JavaScript e specialmente nell&apos;utilizzo di React.js. Ho esperienza
         nella creazione di componenti personalizzati con Material UI e Styled
         Components e nel gestire stati con Redux.
       </p>
-      <p className="mt-5 text-lg text-neutral-500 md:text-xl">
+      <p className="mt-5 text-lg text-neutral-500">
         Inoltre, ho collaborato a progetti che richiedevano l&apos;integrazione
         con API e la gestione del routing delle pagine con Next.js.
       </p>
 
-      <p className="mt-5 text-lg text-neutral-500 md:text-xl">
+      <p className="mt-5 text-lg text-neutral-500">
         Ho iniziato la mia carriera lavorando per la Forneria Messina, dove ho
         mantenuto il sito aziendale con WordPress ed Elementor.
       </p>
-      <p className="mt-5 text-lg text-neutral-500 md:text-xl">
+      <p className="mt-5 text-lg text-neutral-500">
         Successivamente, ho lavorato come frontend developer per The Cove S.R.L,
         dove ho consolidato le mie competenze lavorando su progetti complessi.
       </p>
-      <p className="mt-5 text-lg text-neutral-500 md:text-xl">
+      <p className="mt-5 text-lg text-neutral-500">
         Mi dedico a migliorare continuamente le mie abilità, rimanendo sempre
         aggiornato sulle ultime tecnologie e metodologie di sviluppo. Se vuoi
         saperne di più o discutere di possibili collaborazioni, sentiti libero
         di contattarmi!
       </p>
-      <p className="mt-5 text-lg text-neutral-500 md:text-xl">
+      <p className="mt-5 text-lg text-neutral-500">
         Connettiamoci! Sono sempre aperto a nuove collaborazioni, idee
         interessanti o semplicemente a una chiacchierata sul mondo dello
         sviluppo web. Puoi trovarmi su{' '}
