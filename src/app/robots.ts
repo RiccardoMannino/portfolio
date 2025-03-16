ec38ad496779cd3c55615fb17da327c5ef2c06f1
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2025-03-17 00:12:47 +0100

    sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/*'],
     },
-    sitemap: 'https://riccardomannino.it',
+    sitemap: 'https://riccardomannino.it/sitemap.xml',
   }
 }
